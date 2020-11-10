4659c73826cefe318c5bec9cf27ff5e3f524700f
Heli <helizakay1@gmail.com>
2020-11-10 16:56:53 +0200

    Initial commit

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+//Types 
+import { AnswerObject } from "../App";
+import { Wrapper } from "../App.styles";
+//Styles
+import {ButtonWrapper} from "./QuestionCard.styles";
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
+    questionNum: number;
+    totalQuestions: number;
+}
+const QuestionCard: React.FC<Props> = ({
+    question,
+     answers,
+     callback, 
+     userAnswer, 
+     questionNum, 
+     totalQuestions
+    }) => 
+    (<Wrapper>
+        <p className="number">
+            Question: {questionNum} / {totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question}}></p>
+        <div>
+           {answers.map( (answer) => {
+               return <ButtonWrapper 
+               key={answer}
+               correct={userAnswer?.correctAnswer === answer}
+               userClicked={userAnswer?.answer === answer}
+               >
+                   <button disabled={!!userAnswer}  value={answer} onClick={callback}>
+                       <span dangerouslySetInnerHTML={{ __html: answer}}/>
+                   </button>
+               </ButtonWrapper>
+           })}
+        </div>
+    </Wrapper>);
+
+export default QuestionCard;
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export const shuffleArray = (array: any[]) => {
+    return (
+        [...array].sort(() => {
+            return Math.random() - 0.5;
+        })
+    );
+}
